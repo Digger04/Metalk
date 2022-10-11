58a11a6cb05d7ba2261f5a4b21f52deb27727c9a
Digger04 <thechung04122@gmail.com>
2022-10-12 01:11:52 +0700

    Update confirm otp

---
+++ app/src/main/java/com/example/metalk/UI/Home.java
@@ -0,0 +1,16 @@
+package com.example.metalk.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.metalk.R;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- app/src/main/java/com/example/metalk/UI/Sigup.java
+++ app/src/main/java/com/example/metalk/UI/Sigup.java
@@ -3,6 +3,8 @@ package com.example.metalk.UI;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -13,6 +15,7 @@ import android.widget.Toast;
 
 import com.example.metalk.R;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.FirebaseException;
@@ -28,9 +31,10 @@ import java.util.concurrent.TimeUnit;
 
 public class Sigup extends AppCompatActivity {
 
-    private EditText phone;
+    private EditText phone, password;
     private Button sigup;
     private FirebaseAuth mAuth;
+    private Dialog dialog_cofirmOtp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,27 +89,60 @@ public class Sigup extends AppCompatActivity {
                             public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
 
                                 super.onCodeSent(s, forceResendingToken);
-                                Log.d("ewfwef", "ok");
+
+                                Confirm_Otp(s);
                             }
                         })          // OnVerificationStateChangedCallbacks
                         .build();
         PhoneAuthProvider.verifyPhoneNumber(options);
 
     }
+
+    private void Confirm_Otp(String s) {
+        dialog_cofirmOtp = new Dialog(this);
+        dialog_cofirmOtp.setContentView(R.layout.activity_dialog_cofirm_otp);
+        EditText txt_otp = dialog_cofirmOtp.findViewById(R.id.txt_otp);
+        Button bt_ok = dialog_cofirmOtp.findViewById(R.id.bt_ok);
+
+        bt_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (TextUtils.isEmpty(txt_otp.getText().toString().trim())) {
+                    return;
+                }else {
+                    PhoneAuthCredential credential = PhoneAuthProvider.getCredential(s, txt_otp.getText().toString().trim());
+                    signInWithPhoneAuthCredential(credential);
+                }
+            }
+        });
+
+
+        dialog_cofirmOtp.show();
+    }
+
     private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        View view = findViewById(android.R.id.content);
+
         mAuth.signInWithCredential(credential)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
-
                             FirebaseUser user = task.getResult().getUser();
                             // Update UI
+                            Intent intent = new Intent(getApplicationContext(),Home.class);
+                            startActivity(intent);
                         } else {
-                            // Sign in failed, display a message and update the UI
+                            Snackbar.make(view, "Đăng nhập thất bại", Snackbar.LENGTH_LONG)
+                                    .setTextColor(getResources().getColor(android.R.color.white))
+                                    .setBackgroundTint(getResources().getColor(android.R.color.holo_purple))
+                                    .show();
                             if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
-                                // The verification code entered was invalid
+                                Snackbar.make(view, "Mã xác minh không hợp lệ", Snackbar.LENGTH_LONG)
+                                        .setTextColor(getResources().getColor(android.R.color.white))
+                                        .setBackgroundTint(getResources().getColor(android.R.color.holo_purple))
+                                        .show();
                             }
                         }
                     }
@@ -116,5 +153,6 @@ public class Sigup extends AppCompatActivity {
         phone = findViewById(R.id.phone);
         sigup = findViewById(R.id.button_dangki);
         mAuth = FirebaseAuth.getInstance();
+        password = findViewById(R.id.password);
     }
 }
